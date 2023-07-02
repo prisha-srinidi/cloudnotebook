391c1221ff8e72a56e0133c1733abe2f02361761
Prisha Srinidi <srinidiprisha@gmail.com>
2023-07-02 15:59:59 +0530

    commit 4

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  obj = {
-    a: "this",
-    number: 78,
-  };
-  res.json(obj);
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 });
 
 module.exports = router;
